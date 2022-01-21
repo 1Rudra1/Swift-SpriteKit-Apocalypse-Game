dd6e920743cf6b21e1dbd02d41cbe8bfb5240e6f
Rudra <rudjee2914@gmail.com>
2022-01-21 14:25:52 -0600

    Initial Commit

---
+++ The Apocalypse Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Apocalypse Game
+//
+//  Created by Rudra Patel on 1/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Apocalypse Game/The_Apocalypse_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Apocalypse_GameApp.swift
+//  The Apocalypse Game
+//
+//  Created by Rudra Patel on 1/21/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Apocalypse_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
